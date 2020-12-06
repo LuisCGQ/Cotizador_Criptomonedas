fae3629b75c1d83b626eba1b387e17fbe843d646
Luis García <aig.luis@gmail.com>
2020-12-05 20:25:32 -0600

    Agregada Validacion dle formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import axios from "axios";
+
 import useMoneda from "../hooks/useMoneda";
 import useCriptomoneda from "../hooks/useCriptomoneda";
-import axios from "axios";
+
+import Error from "./Error";
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -25,6 +28,7 @@ const Boton = styled.input`
 const Formulario = () => {
   //state criptomonedas
   const [listacripto, guardarCriptomonedas] = useState([]);
+  const [error, guardarError] = useState(false);
 
   const MONEDAS = [
     { codigo: "USD", nombre: "Dolar de Estados Unidos" },
@@ -55,8 +59,23 @@ const Formulario = () => {
     consultarAPI();
   }, []);
 
+  //Submit
+  const cotizarMoneda = (e) => {
+    e.preventDefault();
+
+    //validar campos llenos
+    if (moneda === "" || criptomoneda === "") {
+      guardarError(true);
+      return;
+    }
+
+    //pasar los datos al componente principal
+    guardarError(false);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={cotizarMoneda}>
+      {error ? <Error mensaje="Todos los campos son Obligatorios" /> : null}
       <SelectMonedas />
       <SelectCripto />
       <Boton type="submit" value="Calcular"></Boton>
