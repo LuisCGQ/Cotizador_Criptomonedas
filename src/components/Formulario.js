7b3ef88f116ea2ac2ffc9a6d76b2812bb9b6eb0d
Luis García <aig.luis@gmail.com>
2020-12-05 20:15:06 -0600

    Formulario Terminado

---
+++ src/components/Formulario.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import useMoneda from "../hooks/useMoneda";
+import useCriptomoneda from "../hooks/useCriptomoneda";
+import axios from "axios";
+
+const Boton = styled.input`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  padding: 10px;
+  background-color: #66a2fe;
+  border: none;
+  width: 100%;
+  border-radius: 10px;
+  color: #fff;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #326ac0;
+    cursor: pointer;
+  }
+`;
+
+const Formulario = () => {
+  //state criptomonedas
+  const [listacripto, guardarCriptomonedas] = useState([]);
+
+  const MONEDAS = [
+    { codigo: "USD", nombre: "Dolar de Estados Unidos" },
+    { codigo: "MXN", nombre: "Peso Mexicano" },
+    { codigo: "EUR", nombre: "Euro" },
+    { codigo: "GBP", nombre: "Libra Esterlina" },
+  ];
+
+  //Utilizar useMoneda
+  const [moneda, SelectMonedas] = useMoneda("Elige tu Moneda", "", MONEDAS);
+
+  //Utilizar useCriptomoneda
+  const [criptomoneda, SelectCripto] = useCriptomoneda(
+    "Elige tu Criptomoneda",
+    "",
+    listacripto
+  );
+
+  //Llamada a la Api
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+      const resultado = await axios.get(url);
+
+      guardarCriptomonedas(resultado.data.Data);
+    };
+    consultarAPI();
+  }, []);
+
+  return (
+    <form action="">
+      <SelectMonedas />
+      <SelectCripto />
+      <Boton type="submit" value="Calcular"></Boton>
+    </form>
+  );
+};
+
+export default Formulario;
